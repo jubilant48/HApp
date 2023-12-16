901237efac7f7917a981b4343db6ab8a063697ad
Михаил Дмитриев <korncl48@gmail.com>
2023-12-16 14:51:58 +0300

    Initial Commit

---
+++ Hotel/HotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelApp.swift
+//  Hotel
+//
+//  Created by macbook on 16.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
